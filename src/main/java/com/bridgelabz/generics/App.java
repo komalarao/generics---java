45a18e545d9e796cbbfd6181576c8aea60eaae79
komalarao <manoharvakamullu1999@gmail.com>
2022-11-30 10:38:03 -0800

    add: welcome message to generics

---
+++ src/main/java/com/bridgelabz/generics/App.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.generics;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to generics program!" );
+    }
+}
